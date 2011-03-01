0f43bcd0e25f988fbeab23c0eecc85113711885c
Bryan Elliott <fordiman@gmail.com>
2011-03-01 00:54:37 -0500

    Fixed reference

--- UseExample.php
+++ UseExample.php
@@ -1,6 +1,6 @@
 <?php
 require_once('ImageDataStore.php');
-$ds = new JpegDataStore('gallery-image.jpg');
+$ds = new ImageDataStore('gallery-image.jpg');
 $ds->writeData((object)array(
 	'comment'=>'You know I like the way this image looks',
 	'taken'=>mktime(),
